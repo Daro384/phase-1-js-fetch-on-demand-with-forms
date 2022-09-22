6e239dde1eedcbf1c1a1c7451dec7245b13d0b88
Joseph <joffan98@gmail.com>
2022-09-22 16:03:58 -0500

    commit tree 4332b6f1d27b995023ec041663b77ae2938dc1d0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const input = document.getElementById("searchByID").value
+    if (["1","2","3"].includes(input)){
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(resp => resp.json())
+        .then(data => {
+            document.querySelector("h4").textContent = data.title
+            document.querySelector("p").textContent = data.summary
+        })
+    } else {
+        document.querySelector("h4").textContent = "404 Error"
+        document.querySelector("p").textContent = "Movie not found"
+    }
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
